7c37957559b36251b6c11244767e213d198c6c83
Manideep Bangaru <manideep@gameopedia.com>
2022-12-07 17:06:09 +0530

    re initialized this branch

--- README.md
+++
@@ -1,2 +0,0 @@
-# dvc
-All about DVC
--- py_api.py
+++
@@ -1,5 +0,0 @@
-import dvc.api
-
-with dvc.api.open("get-started/data.xml",
-                repo="https://github.com/iterative/dataset-registry") as fd:
-    fd.read()
