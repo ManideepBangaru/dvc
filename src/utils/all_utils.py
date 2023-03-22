545f463e7dbb96e99533ec601cbb560ffe444ceb
Manideep Bangaru <manideep@gameopedia.com>
2023-03-22 20:58:42 +0530

    updated few lines of code

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -2,7 +2,7 @@ import yaml
 import os
 
 def read_yaml(path_to_yaml : str) -> dict:
-    with open(path_to_yaml, 'r') as yaml_file:
+    with open(path_to_yaml, 'r', encoding="utf-8") as yaml_file:
         try:
             content = yaml.safe_load(yaml_file)
             return content
