8f439e93fb96ec2bac8f9059fd3a623e203c6f00
ManideepBangaru <bmd994@gmail.com>
2023-03-22 22:32:35 +0530

    corrected setup file

--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ setup(
     description="A small package for testing dvc with ML",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/ManideepBangaru/dvc/tree/ml_use_case",
-    author_email="statmonk@gmail.com",
-    packages="src",
+    url="https://github.com/ManideepBangaru/dvc",
+    author_email="bmd994@gmail.com",
+    packages=["src/"],
     license="GNU",
     python_requires=">=3.8",
     install_requires=[
--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -6,6 +6,7 @@ if __name__ == '__main__':
     args = argparse.ArgumentParser()
 
     args.add_argument('--config', "-c", type=str, default='config.yaml')
+    args.add_argument("--name", "-n", type=str, default="default_name")
 
     parsed_args = args.parse_args()
 
