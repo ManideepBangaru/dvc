284d1ab8683da9b81cf55b6d91ff1951a68d2ed0
Manideep Bangaru <manideep@gameopedia.com>
2023-03-22 20:29:21 +0530

    added package style setup for src

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("Readme.md", "r", encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.3",
+    author="Manideep Bangaru",
+    description="A small package for testing dvc with ML",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ManideepBangaru/dvc/tree/ml_use_case",
+    author_email="statmonk@gmail.com",
+    packages="src",
+    license="GNU",
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "dvc"
+        ]
+)
